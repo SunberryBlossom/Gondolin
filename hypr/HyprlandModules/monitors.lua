b9775100dddf71f9d9bb254fbc932c663d5d6c66
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-06 00:13:39 +0200

    feat(monitors): add default configuration

--- hypr/HyprlandModules/monitors.lua
+++ hypr/HyprlandModules/monitors.lua
@@ -1,6 +1,15 @@
+
+-- My laptop's monitor
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
+    output   = "eDP-1",
+    mode     = "1920x1200@60.00100",
+    position = "0x0",
     scale    = "auto",
 })
+
+-- Good default to fall back to with new monitors
+hl.monitor({
+    output = "",
+    mode = "preferred",
+    position = "auto",
+    scale = 1 })
