2007749ac680549c91b6fb485bdebfaefdff0366
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-05 18:12:16 +0200

    initial commit

---
+++ hypr/hyprland.lua
@@ -0,0 +1,10 @@
+require("HyprlandModules.monitors")
+require("HyprlandModules.autostart")
+require("HyprlandModules.env")
+require("HyprlandModules.rice")
+require("HyprlandModules.inputAndGestures")
+require("HyprlandModules.keybinds")
+require("HyprlandModules.windowsAndWorkspaces")
+
+-- Feel free to copy and use at your own discretion, according to the GPL
+-- // Sun
