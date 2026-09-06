ca78fe94be1417635eec5d12793434831f864899
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-06 16:51:17 +0200

    feat!(environment variables): add env for bibata cursor themes and hyprqt6engine theming

--- hypr/HyprlandModules/env.lua
+++ hypr/HyprlandModules/env.lua
@@ -1,2 +1,8 @@
+-- mouse cursor modifications
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+
+-- enabling hyprqt6engine as theme provider
+hl.env("QT_QPA_PLATFORMTHEME", "hyprqt6engine")
